0f9c1d0c3d5bdfa1f9c4196065ce189055470670
David Remetei <david.remetei@gmail.com>
2024-01-23 12:40:27 +0100

    spring boot project created
    using initializer

---
+++ kesmarki_persons_sb_rest/src/main/java/rd/kesmarki_persons_sb_rest/KesmarkiPersonsSbRestApplication.java
@@ -0,0 +1,13 @@
+package rd.kesmarki_persons_sb_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KesmarkiPersonsSbRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KesmarkiPersonsSbRestApplication.class, args);
+	}
+
+}
---
+++ kesmarki_persons_sb_rest/src/test/java/rd/kesmarki_persons_sb_rest/KesmarkiPersonsSbRestApplicationTests.java
@@ -0,0 +1,13 @@
+package rd.kesmarki_persons_sb_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KesmarkiPersonsSbRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

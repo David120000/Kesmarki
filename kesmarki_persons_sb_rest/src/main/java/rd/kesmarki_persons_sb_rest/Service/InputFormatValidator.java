6770ab1b8d45f2782455d40b3ce9fac9348880f5
David Remetei <david.remetei@gmail.com>
2024-01-23 15:56:42 +0100

    Service/InputFormatValidator util added

---
+++ kesmarki_persons_sb_rest/src/main/java/rd/kesmarki_persons_sb_rest/Service/InputFormatValidator.java
@@ -0,0 +1,84 @@
+package rd.kesmarki_persons_sb_rest.Service;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class InputFormatValidator {
+
+    private final String nameRegex;
+    private final String idCardNoRegex;
+    private final String phoneNumberRegex;
+    private final String emailAddressRegex;
+
+
+    public InputFormatValidator() {
+
+        nameRegex = "^([A-ZÁÉÚŐÓÜÖÍ]([a-záéúőóüöí.]+[ ]?)){2,}$";
+        idCardNoRegex = "^[1-9][0-9]{5}[A-Z]{2}$";
+        phoneNumberRegex = "^[+]?([0-9]+[ -/]?)+[0-9]$";
+        emailAddressRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    }
+
+
+    public boolean validatePersonNameFormat(String personName) {
+
+        Pattern pattern = Pattern.compile(nameRegex);
+        Matcher matcher = pattern.matcher(personName);
+
+        return matcher.find();
+    }
+
+
+    public boolean validateIdCardNumberFormat(String idCardNo) {
+
+        Pattern pattern = Pattern.compile(idCardNoRegex);
+        Matcher matcher = pattern.matcher(idCardNo);
+
+        return matcher.find();
+    }
+
+
+    public boolean validateCountryCode(String countryCode) {
+   
+        boolean validCountryCode = false;
+
+        if(countryCode.length() == 2 && countryCode.equals(countryCode.toUpperCase())) {
+
+            String[] countryCodes = Locale.getISOCountries();
+
+            for(int i = 0; i < countryCodes.length; i++) {
+            
+                if(countryCodes[i].equals(countryCode)) {
+                    validCountryCode = true;
+                    break;
+                }
+            }
+
+        }  
+
+        return validCountryCode;
+    }
+
+
+    public boolean validatePhoneNumberFormat(String phoneNumber) {
+
+        Pattern pattern = Pattern.compile(phoneNumberRegex);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        
+        return matcher.find();
+    }
+
+
+    public boolean validateEmailAddressFormat(String emailAddress) {
+
+        Pattern pattern = Pattern.compile(emailAddressRegex);
+        Matcher matcher = pattern.matcher(emailAddress);
+
+        return matcher.find();
+    }
+    
+}
